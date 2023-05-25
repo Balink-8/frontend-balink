60bc335b21e0425527a2d407983ce24693570fa9
m29mujib <mamujib29@gmail.com>
2023-05-25 10:34:17 +0700

    add login pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ComponentExample from "./components/ComponentExample";
+import Login from '../src/components/Login/Login'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ComponentExample />} />
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
   );
