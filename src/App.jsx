ca30a998ea7e50b76b977b7ff3d33eccafa1a595
dzakwanpxr <87738561+dzakwanpxr@users.noreply.github.com>
2023-06-01 04:47:00 +0700

    add modal pop up component

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,34 @@ import Layout from "./components/Layout/Layout";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Akun from "./pages/Akun/Akun";
-import TransaksiEvent from "./pages/Transaksi/TransaksiEvent/TransaksiEvent"
+import TransaksiEvent from "./pages/Transaksi/TransaksiEvent/TransaksiEvent";
 import TransaksiProduk from "./pages/Transaksi/TransaksiProduk/Transaksiproduk";
+import { ModalAkunContextProvider } from "./context/ModalAkunContext";
+import { ModalContextProvider } from "./context/ModalContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/akun" element={<Akun />} />
-          <Route path="/event" element={<h1>event</h1>} />
-          <Route path="/produk" element={<h1>produk</h1>} />
-          <Route path="/kategori" element={<h1>kategori</h1>} />
-          <Route path="/transaksi" element={<TransaksiEvent/>} />
-          <Route path="/transaksi/produk" element={<TransaksiProduk/>} />
-          <Route path="/promo" element={<h1>promo</h1>} />
-          <Route path="/artikel" element={<h1>artikel</h1>} />
-          <Route path="/profile" element={<h1>profile</h1>} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
+      <ModalAkunContextProvider>
+        <ModalContextProvider>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/akun" element={<Akun />} />
+              <Route path="/event" element={<h1>event</h1>} />
+              <Route path="/produk" element={<h1>produk</h1>} />
+              <Route path="/kategori" element={<h1>kategori</h1>} />
+              <Route path="/transaksi" element={<TransaksiEvent />} />
+              <Route path="/transaksi/produk" element={<TransaksiProduk />} />
+              <Route path="/promo" element={<h1>promo</h1>} />
+              <Route path="/artikel" element={<h1>artikel</h1>} />
+              <Route path="/profile" element={<h1>profile</h1>} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </ModalContextProvider>
+      </ModalAkunContextProvider>
     </BrowserRouter>
   );
 }
--- src/pages/Transaksi/TransaksiEvent/TransaksiEvent.jsx
+++ src/pages/Transaksi/TransaksiEvent/TransaksiEvent.jsx
@@ -2,97 +2,101 @@ import React from "react";
 import TableEvent from "../../../components/Table/TableTransaksi/TransaksiEvent/TableEvent";
 
 const TransaksiEvent = () => {
-    const userDataEvent = [
-      {
-        kodetransaksi: "ABCD1234E5F6",
-        username: "JasMie77",
-        waktu: "15/12/23 - 20:40:56",
-        event: "Tari Kecak",
-        total: "Rp 110.000 ",
-        status: "Sukses",
-      },
-      {
-        kodetransaksi: "ABCD1234E5F6",
-        username: "JasMie77",
-        waktu: "15/12/23 - 20:40:56",
-        event: "Tari Kecak",
-        total: "Rp 110.000 ",
-        status: "Dipesan",
-      },
-      {
-        kodetransaksi: "ABCD1234E5F6",
-        username: "JasMie77",
-        waktu: "15/12/23 - 20:40:56",
-        event: "Tari Kecak",
-        total: "Rp 110.000 ",
-        status: "Dipesan",
-      },
-      {
-        kodetransaksi: "ABCD1234E5F6",
-        username: "JasMie77",
-        waktu: "15/12/23 - 20:40:56",
-        event: "Tari Kecak",
-        total: "Rp 110.000 ",
-        status: "Dibatalkan",
-      },
-      {
-        kodetransaksi: "ABCD1234E5F6",
-        username: "JasMie77",
-        waktu: "15/12/23 - 20:40:56",
-        event: "Tari Kecak",
-        total: "Rp 110.000 ",
-        status: "Sukses",
-      },
-      {
-        kodetransaksi: "ABCD1234E5F6",
-        username: "JasMie77",
-        waktu: "15/12/23 - 20:40:56",
-        event: "Tari Kecak",
-        total: "Rp 110.000 ",
-        status: "Sukses",
-      },
-      {
-        kodetransaksi: "ABCD1234E5F6",
-        username: "JasMie77",
-        waktu: "15/12/23 - 20:40:56",
-        event: "Tari Kecak",
-        total: "Rp 110.000 ",
-        status: "Sukses",
-      },
-      {
-        kodetransaksi: "ABCD1234E5F6",
-        username: "JasMie77",
-        waktu: "15/12/23 - 20:40:56",
-        event: "Tari Kecak",
-        total: "Rp 110.000 ",
-        status: "Sukses",
-      },
-      {
-        kodetransaksi: "ABCD1234E5F6",
-        username: "JasMie77",
-        waktu: "15/12/23 - 20:40:56",
-        event: "Tari Kecak",
-        total: "Rp 110.000 ",
-        status: "Sukses",
-      },
-      {
-        kodetransaksi: "ABCD1234E5F6",
-        username: "JasMie77",
-        waktu: "15/12/23 - 20:40:56",
-        event: "Tari Kecak",
-        total: "Rp 110.000 ",
-        status: "Sukses",
-      },
-      {
-        kodetransaksi: "ABCD1234E5F6",
-        username: "JasMie77",
-        waktu: "15/12/23 - 20:40:56",
-        event: "Tari Kecak",
-        total: "Rp 110.000 ",
-        status: "Sukses",
-      },
-    ];
-    return <TableEvent userDataEvent={userDataEvent} />;
-  };
-  
-  export default TransaksiEvent;
+  const userDataEvent = [
+    {
+      kodetransaksi: "ABCD1234E5F6",
+      username: "JasMie77",
+      waktu: "15/12/23 - 20:40:56",
+      event: "Tari Kecak",
+      total: "Rp 110.000 ",
+      status: "Sukses",
+    },
+    {
+      kodetransaksi: "ABCD1234E5F6",
+      username: "JasMie77",
+      waktu: "15/12/23 - 20:40:56",
+      event: "Tari Kecak",
+      total: "Rp 110.000 ",
+      status: "Dipesan",
+    },
+    {
+      kodetransaksi: "ABCD1234E5F6",
+      username: "JasMie77",
+      waktu: "15/12/23 - 20:40:56",
+      event: "Tari Kecak",
+      total: "Rp 110.000 ",
+      status: "Dipesan",
+    },
+    {
+      kodetransaksi: "ABCD1234E5F6",
+      username: "JasMie77",
+      waktu: "15/12/23 - 20:40:56",
+      event: "Tari Kecak",
+      total: "Rp 110.000 ",
+      status: "Dibatalkan",
+    },
+    {
+      kodetransaksi: "ABCD1234E5F6",
+      username: "JasMie77",
+      waktu: "15/12/23 - 20:40:56",
+      event: "Tari Kecak",
+      total: "Rp 110.000 ",
+      status: "Sukses",
+    },
+    {
+      kodetransaksi: "ABCD1234E5F6",
+      username: "JasMie77",
+      waktu: "15/12/23 - 20:40:56",
+      event: "Tari Kecak",
+      total: "Rp 110.000 ",
+      status: "Sukses",
+    },
+    {
+      kodetransaksi: "ABCD1234E5F6",
+      username: "JasMie77",
+      waktu: "15/12/23 - 20:40:56",
+      event: "Tari Kecak",
+      total: "Rp 110.000 ",
+      status: "Sukses",
+    },
+    {
+      kodetransaksi: "ABCD1234E5F6",
+      username: "JasMie77",
+      waktu: "15/12/23 - 20:40:56",
+      event: "Tari Kecak",
+      total: "Rp 110.000 ",
+      status: "Sukses",
+    },
+    {
+      kodetransaksi: "ABCD1234E5F6",
+      username: "JasMie77",
+      waktu: "15/12/23 - 20:40:56",
+      event: "Tari Kecak",
+      total: "Rp 110.000 ",
+      status: "Sukses",
+    },
+    {
+      kodetransaksi: "ABCD1234E5F6",
+      username: "JasMie77",
+      waktu: "15/12/23 - 20:40:56",
+      event: "Tari Kecak",
+      total: "Rp 110.000 ",
+      status: "Sukses",
+    },
+    {
+      kodetransaksi: "ABCD1234E5F6",
+      username: "JasMie77",
+      waktu: "15/12/23 - 20:40:56",
+      event: "Tari Kecak",
+      total: "Rp 110.000 ",
+      status: "Sukses",
+    },
+  ];
+  return (
+    <>
+      <TableEvent userDataEvent={userDataEvent} />
+    </>
+  );
+};
+
+export default TransaksiEvent;
