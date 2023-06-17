e1d77771b477db3458e7032a0a63ee06c7768138
dzakwanpxr <87738561+dzakwanpxr@users.noreply.github.com>
2023-06-18 00:28:59 +0700

    removing unecessary import on artikel.jsx

--- src/pages/Artikel/Artikel.jsx
+++ src/pages/Artikel/Artikel.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import TableArtikel from "../../components/Table/TableArtikel/TableArtikel";
-import Spinner from "../../components/Spinner/Spinner";
-import ErrorDisplay from "../../components/ErrorDisplay/ErrorDisplay";
 
 const Artikel = () => {
   return (
