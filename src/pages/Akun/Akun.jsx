df009463ee477806d9ec957f334a160fec7fe245
dzakwanpxr <87738561+dzakwanpxr@users.noreply.github.com>
2023-06-18 00:29:42 +0700

    fixing table data render and pagination in akun page

--- src/pages/Akun/Akun.jsx
+++ src/pages/Akun/Akun.jsx
@@ -1,33 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import TableAkun from "../../components/Table/TableAkun/TableAkun";
-import useApi from "../../api/useApi";
-import Spinner from "../../components/Spinner/Spinner";
-import EmptyTable from "../../components/EmptyTable/EmptyTable";
-import ErrorDisplay from "../../components/ErrorDisplay/ErrorDisplay";
 
 const Akun = () => {
-  const { response, error, loading, get } = useApi();
-
-  useEffect(() => {
-    get("/user").catch((error) => {
-      // Handle error
-      console.error(error);
-    });
-  }, []);
-
   return (
     <div>
-      <div>
-        {loading ? (
-          <Spinner />
-        ) : error ? (
-          <ErrorDisplay errorMessage={error.message} />
-        ) : response?.data?.data?.length === 0 ? (
-          <EmptyTable />
-        ) : (
-          <TableAkun userData={response?.data?.data} />
-        )}
-      </div>
+      <TableAkun />
     </div>
   );
 };
